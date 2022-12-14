4b9e70c100f2b3b2cac3d232d237d76a720ce06e
Alvin Mithamo <alvinmithamo100@gmail.com>
2022-12-14 11:31:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function countDown(start) {
+    // Make sure the input is a positive integer
+    if (start <= 0 || start % 1 !== 0) {
+      return;
+    }
+  
+    console.log(start);
+  
+    // If the starting number is not already 0, call countDown again with the starting number decremented by 1
+    if (start > 0) {
+      countDown(start - 1);
+    }
+  }
+  
+  
+
+  
+  
